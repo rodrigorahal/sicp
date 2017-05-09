72b03a28de09d50caa298393eda383386ab0f780
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-05-09 12:29:49 -0300

    add exercise 2.19 solution

---
+++ ch2/2.19.scm
@@ -0,0 +1,32 @@
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(cc 100 us-coins)
+;=> 292
+
+(define reversed-us-coins (list 1 5 10 25 50))
+
+(cc 100 reversed-us-coins)
+;=> 292
+
+; The order of the list coin-values does not matter.
+; Every sub-list is evaluated.
