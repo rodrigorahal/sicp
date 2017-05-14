1d1c52e32163dbe6b4202831f2c731c433520fa5
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-05-14 00:49:18 -0300

    add exercise 2.37 solution

---
+++ ch2/2.36.scm
@@ -0,0 +1,15 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map (lambda (seq) (car seq)) seqs))
+            (accumulate-n op init (map (lambda (seq) (cdr seq)) seqs)))))
+
+(list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
