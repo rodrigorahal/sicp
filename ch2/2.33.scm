6dd914440eee70593959e9a3eb7e9b7c6d025ab6
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-05-13 23:04:40 -0300

    add exercise 2.33 solution

---
+++ ch2/2.33.scm
@@ -0,0 +1,22 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(map (lambda (x) (* 2 x)) (list 1 2 3 4))
+;=> (2 4 6 8)
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(append (list 1 2 3) (list 4 5 6))
+;=> (1 2 3 4 5 6)
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+(length (list 1 2 3))
