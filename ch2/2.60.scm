9521ced05a8a1ad42d066a3c0bf964db2e974d11
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-05-22 19:21:47 -0300

    add exercise 2.60 solution

---
+++ ch2/2.60.scm
@@ -0,0 +1,24 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (append set1 set2))
+
+(union-set '(1 2 3) '(1 2 3))
+;=> (1 2 3 1 2 3)
+
+; By allowing duplicates adjoin-set and union-set now run in O(1).
+; We don't change the implementation of element-of-set and intersection-set
+; so they stil run in O(n) and O(n2)
