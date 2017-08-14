7895118901afd9fa900c49e0573d732e7354653d
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-08-14 20:54:23 -0300

    add exercise 3.61 solution

---
+++ ch3/3.61.scm
@@ -0,0 +1,15 @@
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1)
+                  (stream-car s2))
+               (add-streams (scale-stream (stream-cdr s1) (stream-car s2))
+                            (mul-series (stream-cdr s1) s2))))
+
+; X = 1 - SR.X
+
+(define (invert-unit-series s)
+  (cons-stream 1 (scale-stream (mul-series (stream-cdr s)
+                                           (invert-unit-series s))
+                               -1)))
