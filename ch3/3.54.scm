e2e6e516515772ec4b0e9f55d484e7aa783b9e06
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-08-14 19:40:13 -0300

    add exercise 3.54 solution

---
+++ ch3/3.54.scm
@@ -0,0 +1,10 @@
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define ones (cons-stream 1 ones))
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(define factorials (cons-stream 1 (mul-streams factorials (stream-cdr integers))))
