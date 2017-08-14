e42f1fb76b835dcf16dabc07d06eed51645653de
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-08-14 20:13:01 -0300

    add exercise 3.56 solution

---
+++ ch3/3.56.scm
@@ -0,0 +1,22 @@
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+; this is actually merge-sort
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream s1car (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream s2car (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+
+(define S (cons-stream 1 (merge (merge (scale-stream S 2)
+                                       (scale-stream 3))
+                                (scale-stream 5))))
