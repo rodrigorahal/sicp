8720aea3fe2b0eb120fd615f25fa30cca02e673f
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-08-14 18:17:01 -0300

    add exercise 3.50 solution

---
+++ ch3/3.50.scm
@@ -0,0 +1,15 @@
+; one argument map
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+; multiple arguments map
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
