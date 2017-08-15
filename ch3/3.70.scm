7d96b242058786138bf49ae72954588f5a65e5ab
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-08-15 12:04:02 -0300

    add exercise 3.70 solution

---
+++ ch3/3.70.scm
@@ -0,0 +1,51 @@
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream s1car (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream s2car (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< (weight s1car) (weight s2car))
+                  (cons-stream s1car (merge-weighted (stream-cdr s1) s2 weight)))
+                 ((> (weight s1car) (weight s2car))
+                  (cons-stream s2car (merge-weighted s1 (stream-cdr s2) weight)))
+                 (else
+                  (cons-stream s1car
+                               (merge-weighted (stream-cdr s1)
+                                      (stream-cdr s2) weight))))))))
+
+(define (weighted-pairs s t weight)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (merge-weighted
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (weighted-pairs (stream-cdr s) (stream-cdr t) weight)
+    weight)))
+
+(weighted-pairs integers
+                  integers
+                  (lambda (x)
+                    (apply + x))))
