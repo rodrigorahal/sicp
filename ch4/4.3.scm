e32565dd9078bd1f4bf0b3d84a53d2b3abfe2094
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-08-24 10:58:54 -0300

    add exercise 4.3 solution

---
+++ ch4/4.3.scm
@@ -0,0 +1,37 @@
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((get 'eval (operator exp))
+          ((get 'eval (operator exp)) exp env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+; procedures are installed in the table 'eval
+; new procedures can be installed in the lookup-table without touching
+; eval's definition
