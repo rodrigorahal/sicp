1875b7305606b4f775f6722d459abfeb9f17aec2
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-09-04 12:18:29 -0300

    add exercise 4.37 solution

---
+++ ch4/4.37.scm
@@ -0,0 +1,13 @@
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high))
+        (hsq (* high high)))
+    (let ((j (an-integer-between i high)))
+      (let ((ksq (+ (* i i) (* j j))))
+        (require (>= hsq ksq))
+        (let ((k (sqrt ksq)))
+          (require (integer? k))
+          (list i j k))))))
+
+; Ben defines two additional requires that reduce the number of searches.
+; (require (>= hsq ksq)) and (require (integer? k)), so yes Ben's version is
+; more efficient.
