59e40c51ea52aa3b475987e1c506c253d82e1132
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-09-11 15:27:51 -0300

    add exercise 4.74 solution

---
+++ ch4/4.74.scm
@@ -0,0 +1,18 @@
+(define (stream-flatmap proc s)
+  (flatten-stream (stream-map proc s)))
+
+(define (flatten-stream stream)
+  (if (stream-null? stream)
+      the-empty-stream
+      (interleave-delayed
+       (stream-car stream)
+       (delay (flatten-stream (stream-cdr stream))))))
+
+(define (simple-stream-flatmap proc s)
+  (simple-flatten (stream-map proc s)))
+
+(define (simple-flatten stream)
+  (stream-map stream-car
+              (stream-filter (lambda (stream)
+                                (not (stream-null? stream)))
+                             stream)))
