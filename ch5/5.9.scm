66661d25106d138a2ddaa48b6105f9ca207787a0
Rodrigo Rahal <rodrigo.rahal@hotmail.com>
2017-09-27 14:51:43 -0300

    add exercise 5.9 solution

---
+++ ch5/5.9.scm
@@ -0,0 +1,10 @@
+(define (make-operation-exp exp machine labels operations)
+  (let ((op (lookup-prim (operation-exp-op exp) operations))
+        (aprocs
+         (map (lambda (e)
+                (if (or (register-exp? e) (constant-exp? e))
+                    (make-primitive-exp e machine labels)
+                    (error "Operations with registers and constants only -- ASSEMBLE" e)))
+              (operation-exp-operands exp))))
+    (lambda ()
+      (apply op (map (lambda (p) (p)) aprocs)))))
